07898de2cba6603bc890d4b029fa80261708b3b2
Nandhini S <nandhini18vgr@gmail.com>
2023-07-11 12:20:17 +0530

    simplified

--- src/LibraryDataManagementApp.java
+++ src/LibraryDataManagementApp.java
@@ -1,84 +1,41 @@
-import java.util.HashMap;
-import java.util.Scanner;
 class LibraryItem{
-    private int id;
+    private int id; //id is alphanumeric
     String title;
     int year;
+
     public int getId() {
         return id;
     }
+
     public void setId(int id) {
         this.id = id;
     }
 }
 class Book extends LibraryItem{
-    HashMap<Integer, String> bk = new HashMap<>();
     String author;
-    int i = 1;
-    public void addBkDetail(){
-        bk.put(i, "Author name: "+author+"\nBook Title: "+title+"\nBook ID: "+getId()+"\nPublication year: "+year);
-        i++;
-    }
-    public void printLastIssuedBook(){
-        System.out.println(bk.get(bk.size()));
+    public void printBook(int id, String title, int year, String author){
+        System.out.println(id+" "+title+" "+year+" "+author);
     }
 }
 class Magazine extends LibraryItem{
-    HashMap<Integer, String> mag = new HashMap<>();
-    int issue = 0, i = 1;
-    public void addMagDetail(){
-        mag.put(i, "Number of issues: "+issue+"\nMagazine Title: "+title+"\nMagazine ID: "+getId()+"\nPublication year: "+year);
-        i++;
-    }
-    public void printLastIssuedMagazine(){
-        System.out.println(mag.get(mag.size()));
+    int issue;
+    public void printMagazine(int id, String title, int year, int issue){
+        System.out.println(id+" "+title+" "+year+" "+issue);
     }
 }
 public class LibraryDataManagementApp {
     public static void main(String[] args) {
-        int choice = 0;
-        Book book = new Book();
-        Magazine magazine = new Magazine();
-        Scanner scanner = new Scanner(System.in);
-        while (choice>=0){
-            System.out.println("\n\n\tWhat function do you want to perform?\n1.) Load books detail of particular author\n2.) Issues particular book\n3.)Last issued book\n4.)Last issued magazine\n\n");
-            System.out.print("Enter your choice (1 to 4) or -1 to exit: ");
-            choice = scanner.nextInt();
-            switch (choice) {
-                case 1 -> {
-                    System.out.print("Enter author name: ");
-                    book.author = scanner.nextLine();
-                    scanner.nextLine();
-                    System.out.print("Enter Book ID: ");
-                    book.setId(scanner.nextInt());
-                    System.out.print("Enter Book title: ");
-                    scanner.nextLine();
-                    book.title = scanner.nextLine();
-                    System.out.print("Enter year of publication: ");
-                    book.year = scanner.nextInt();
-                    System.out.println("Book Loaded to DataBase successfully");
-                    book.addBkDetail();
-                }
-                case 2 -> {
-                    System.out.print("Enter Magazine title: ");
-                    scanner.nextLine();
-                    magazine.title = scanner.nextLine();
-                    System.out.print("Do you want to issue it (y/n): ");
-                    char ch = scanner.next().charAt(0);
-                    if (ch == 'y') {
-                        magazine.issue++;
-                    }
-                    System.out.print("Enter Magazine ID: ");
-                    magazine.setId(scanner.nextInt());
-                    System.out.print("Enter Magazine publication year: ");
-                    magazine.year = scanner.nextInt();
-                    System.out.println("Magazine issued successfully");
-                    magazine.addMagDetail();
-                }
-                case 3 -> book.printLastIssuedBook();
-                case 4 -> magazine.printLastIssuedMagazine();
-            }
-        }
-        scanner.close();
+        Book alchemist = new Book();
+        alchemist.setId(101);
+        alchemist.title = "The Alchemist";
+        alchemist.year = 1940;
+        alchemist.author = "Paul";
+        alchemist.printBook(alchemist.getId(), alchemist.title, alchemist.year, alchemist.author);
+        Magazine fortNight = new Magazine();
+        fortNight.setId(202);
+        fortNight.title = "The FortNight";
+        fortNight.year = 2022;
+        fortNight.issue = 12;
+        fortNight.printMagazine(fortNight.getId(), fortNight.title, fortNight.year, fortNight.issue);
     }
 }
